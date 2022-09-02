07932df4a2b60dbac22e109470257bd7aa8b0f73
Sri Lasya Addala <73516622+SriLasya09@users.noreply.github.com>
2022-09-02 11:32:40 +0530

    c

---
+++ conditions/index.py
@@ -0,0 +1,67 @@
+a=7
+if(a==7):
+    print("true")
+else:
+    print("false")    
+
+
+i=8
+if(i>=8):
+    print("true")
+else:
+    print('false')    
+    
+b=5
+if(b!=5):
+    print("true")
+else:
+    print('false')    
+    
+#branching
+age = 17
+if(age > 18):  
+    print('you can enter')
+print('move on')    
+
+#if-else
+
+age1=19
+if(age>18):
+    print('you can enter')
+else:
+    print('go see meat loaf')
+print('move on') 
+
+#elif
+age2=18
+if(age2>18):
+    print("you can enter")   
+elif(age2==18):
+    print("go see Pink Floyd")
+else:
+    print("go see meat loaf")
+print("move on")               
+   
+#logic operators
+
+print(not(True))  
+print(not(False)) 
+
+A=False
+B=False
+print(A or B)
+print(A & B)
+
+album_year = 1990
+if(album_year<1980) or (album_year>1989):
+    print("The album was made in the 70's or 90's ")
+else:
+    print("The album was made in the 1980's '")    
+
+album_year1= 1980
+if(album_year1>1979)and(album_year1<1990):
+    print("This album was made in the 80's ")
+else:
+    print("made in the other years")
+
+    
